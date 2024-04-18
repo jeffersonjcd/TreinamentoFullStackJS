65016e1f27a4270528ef71e0a30516bddfcf7f27
jefferson.divino <jefferson.divino@gerencianet.com.br>
2024-04-18 18:26:36 -0300

    Aula sobre recursos nativos do navegador

---
+++ javascript/aula12/index.js
@@ -0,0 +1,14 @@
+let nome = "Jefferson"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt("Insira o seu nome: ")
+//console.log(nome)
+
+//confirmacao = window.confirm("Confirma essa ação?")
+//console.log(confirmacao)
+
+alert("Olá mundo!")
