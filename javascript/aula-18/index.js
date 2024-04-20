960116b79f09d5adfd378193836b16484c42701b
jefferson.divino <jefferson.divino@gerencianet.com.br>
2024-04-20 15:33:06 -0300

    Aula sobre if else e operador ternario

---
+++ javascript/aula-18/index.js
@@ -0,0 +1,16 @@
+// const idade = window.prompt("Informe a sua idade: ")
+
+// if ( idade > 18 ) {
+//     alert("Você é maior de idade")
+// } else if(idade > 12) {
+//     alert("Você é menor de idade")
+// }else if (idade < 4){
+//     alert("Você é criança")
+// }else {
+//     alert("..")
+// }
+
+
+// Operador ternário
+const resultado = (6===6) ? "Verdadeiro" : "Falso"
+console.log("Resultado")
